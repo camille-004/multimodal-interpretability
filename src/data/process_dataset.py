327ecef882c4a4827bb722e8fce5163b46fc1aac
camille-004 <dunningcamille@gmail.com>
2022-04-18 02:33:21 -0700

    Docstrings

--- src/data/process_dataset.py
+++ src/data/process_dataset.py
@@ -5,11 +5,19 @@ import torch.utils.data as data
 
 
 class QADataset(data.Dataset):
+    """A Dataset object for question-answer pairs, will soon add images."""
+
     def __init__(self, data_fp: str):
         self.data = pd.read_csv(data_fp)
         self.data_len = len(self.data)
 
     def __getitem__(self, item: int) -> Dict:
+        """
+        Retrieve an item from the dataset
+
+        :param item: Index of the item to retrieve
+        :return: The question, answer, and image name in dictionary form
+        """
         assert (
             0 <= item < self.data_len
         ), "Specified index is out of range for this dataset"
@@ -21,4 +29,5 @@ class QADataset(data.Dataset):
         }
 
     def __len__(self) -> int:
+        """Get the nimber of examples in this dataset"""
         return self.data_len
